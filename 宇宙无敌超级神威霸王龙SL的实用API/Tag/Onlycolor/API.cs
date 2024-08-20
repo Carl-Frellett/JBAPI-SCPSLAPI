becc7070505f81ba7c679bacef12129e85f4ea42
Carl-Frellett <2033200993@qq.com>
2024-08-20 08:31:32 +0800

    写的什么他妈玩意

--- 宇宙无敌超级神威霸王龙SL的实用API/Tag/Onlycolor/API.cs
+++ 宇宙无敌超级神威霸王龙SL的实用API/Tag/Onlycolor/API.cs
@@ -1,5 +1,6 @@
 ﻿using Exiled.API.Features;
 using JBPI.Log;
+using MEC;
 
 namespace JBAPI.Tag
 {
@@ -7,10 +8,12 @@ namespace JBAPI.Tag
     {
         public static void ORTag(this Player 玩家, string 文本, string 颜色)
         {
-            玩家.RankName = 文本;
-            玩家.RankColor = 颜色;
-
-            LogAPI.日志($"JBAPI.OnlyColor调用 玩家 {玩家.Nickname} ({玩家.UserId})");
+            Timing.CallContinuously(2f, () =>
+            {
+                玩家.RankName = 文本;
+                玩家.RankColor = 颜色;
+                LogAPI.日志($"JBAPI.OnlyColor调用 玩家 {玩家.Nickname} ({玩家.UserId})");
+            });
         }
     }
 }
--- 宇宙无敌超级神威霸王龙SL的实用API/Tag/somecolor/API.cs
+++ 宇宙无敌超级神威霸王龙SL的实用API/Tag/somecolor/API.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using JBAPI.UnityScript;
 using JBPI.Log;
+using MEC;
 
 namespace JBAPI.Tag
 {
@@ -20,31 +21,36 @@ namespace JBAPI.Tag
 
         public static void RTag(this Player 玩家, string 文本, bool 是否启用)
         {
-            玩家.RankName = 文本;
-
-            if (!是否启用)
+            Timing.CallContinuously(2f, () =>
             {
-                var 神威 = 玩家.GameObject.GetComponent<TagController>();
-                if (神威 != null)
+                玩家.RankName = 文本;
+
+                if (!是否启用)
                 {
-                    Object.Destroy(神威);
+                    var 神威 = 玩家.GameObject.GetComponent<TagController>();
+                    if (神威 != null)
+                    {
+                        Object.Destroy(神威);
+                    }
+
+                    玩家.RankColor = "red";
+
+                    return;
                 }
 
-                玩家.RankColor = "red";
+                var 霸王龙 = 玩家.GameObject.GetComponent<TagController>();
 
-                return;
-            }
+                LogAPI.日志($"JBAPI.somecolor调用 玩家 {玩家.Nickname} ({玩家.UserId})");
+                LogAPI.自定义("111", System.ConsoleColor.Red);
 
-            var 霸王龙 = 玩家.GameObject.GetComponent<TagController>();
+                if (霸王龙 == null)
+                {
+                    霸王龙 = 玩家.GameObject.AddComponent<TagController>();
+                    霸王龙.Colors = 颜色;
+                    霸王龙.Interval = 0.5f;//孩子，时间越快服务器越容易崩，如果你改了，服务器爆炸概不负责
+                }
 
-            LogAPI.日志($"JBAPI.somecolor调用 玩家 {玩家.Nickname} ({玩家.UserId})");
-
-            if (霸王龙 == null)
-            {
-                霸王龙 = 玩家.GameObject.AddComponent<TagController>();
-                霸王龙.Colors = 颜色;
-                霸王龙.Interval = 0.5f;//孩子，时间越快服务器越容易崩，如果你改了，服务器爆炸概不负责
-            }
+            });
         }
     }
 }
