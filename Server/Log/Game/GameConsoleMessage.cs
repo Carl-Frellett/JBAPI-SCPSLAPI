a0534c8156aba290d902f13e713199bf3b866d59
卡尔·弗雷利特 <2033200993@qq.com>
2024-10-05 22:03:47 +0800

    N

---
+++ Server/Log/Game/GameConsoleMessage.cs
@@ -0,0 +1,54 @@
+﻿using Exiled.API.Features;
+using static JBAPI.Log.Level;
+
+namespace JBAPI.Log
+{
+    public static class GameCC
+    {
+    /// <summary>
+    /// 一个可以向游戏控制台发送消息的API
+    /// </summary>
+    /// <param name="玩家"></param>
+    /// <param name="消息"></param>
+    /// <param name="级别"></param>
+        public static void 控制台消息(this Player 玩家, string 消息, ConsoleLevel 级别)
+        {
+            string 前缀 = _级别(级别);
+            string 颜色 = _颜色(级别);
+
+            玩家.SendConsoleMessage($"<color={颜色}>{前缀}</color>{消息}", "");
+        }
+
+        private static string _级别(ConsoleLevel 级别)
+        {
+            switch (级别)
+            {
+                case ConsoleLevel.消息:
+                    return "[消息] ";
+                case ConsoleLevel.警告:
+                    return "[警告] ";
+                case ConsoleLevel.错误:
+                    return "[错误] ";
+                case ConsoleLevel.自定义:
+                    return "";
+                default:
+                    return "";
+            }
+        }
+
+        private static string _颜色(ConsoleLevel 级别)
+        {
+            switch (级别)
+            {
+                case ConsoleLevel.消息:
+                    return "#a2a2a2";
+                case ConsoleLevel.警告:
+                    return "yellow";
+                case ConsoleLevel.错误:
+                    return "red";
+                default:
+                    return "";
+            }
+        }
+    }
+}
---
+++ Server/Log/ServerConsoleMessage.cs
@@ -0,0 +1,48 @@
+﻿namespace JBPI.Log
+{
+    public static class ServerCC
+    {
+        /// <summary>
+        /// 默认情况下的日志
+        /// </summary>
+        /// <param name="消息"></param>
+        public static void 日志(string 消息)
+            => ServerConsole.AddLog(消息, System.ConsoleColor.Cyan);
+
+        /// <summary>
+        /// 警告日志
+        /// </summary>
+        /// <param name="消息"></param>
+        public static void 警告(string 消息)
+            => ServerConsole.AddLog(消息, System.ConsoleColor.DarkYellow);
+
+        /// <summary>
+        /// 调式日志 Debug
+        /// </summary>
+        /// <param name="消息"></param>
+        public static void 调试(string 消息)
+            => Exiled.API.Features.Log.Debug(消息);
+
+        /// <summary>
+        /// 发生错误时的日志
+        /// </summary>
+        /// <param name="消息"></param>
+        public static void 错误(string 消息)
+            => ServerConsole.AddLog($"[错误] {消息}", System.ConsoleColor.Red);
+
+        /// <summary>
+        /// 其他日志
+        /// </summary>
+        /// <param name="消息"></param>
+        public static void 其他(string 消息)
+            => ServerConsole.AddLog(消息, System.ConsoleColor.Green);
+
+        /// <summary>
+        /// 自定义消息
+        /// </summary>
+        /// <param name="消息"></param>
+        /// <param name="颜色"></param>
+        public static void 自定义(string 消息, System.ConsoleColor 颜色)
+            => ServerConsole.AddLog(消息, 颜色);
+    }
+}
